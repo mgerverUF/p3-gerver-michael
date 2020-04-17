6cc3282a50078b76a1d99c91b322a932962e6342
Michael Gerver <mgerver@ufl.edu>
2020-04-17 16:07:42 -0400

    Build lunch array and add images to repo

---
+++ src/modules/lunch-generator.js
@@ -0,0 +1,51 @@
+const lunchArray = [
+    {
+        lunchName: 'Pizza',
+        lunchPic: 'images/'
+    },
+
+    {
+        lunchName: 'Noodle Bowl',
+        lunchPic: 'images/'
+    },
+
+    {
+        lunchName: 'Burrito',
+        lunchPic: 'images/'
+    },
+
+    {
+        lunchName: 'Chicken and Fries',
+        lunchPic: 'images/'
+    },
+
+    {
+        lunchName: 'Burger and Fries',
+        lunchPic: 'images/'
+    },
+
+    {
+        lunchName: 'Crab Cakes',
+        lunchPic: 'images/'
+    },
+
+    {
+        lunchName: 'Yogurt Parfait',
+        lunchPic: 'images/'
+    },
+
+    {
+        lunchName: 'Grilled Chicken',
+        lunchPic: 'images/'
+    },
+
+    {
+        lunchName: 'Boiled Egg',
+        lunchPic: 'images/'
+    },
+
+    {
+        lunchName: 'Banana Smoothie',
+        lunchPic: 'images/'
+    }
+]
