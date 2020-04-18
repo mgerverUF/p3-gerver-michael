cf7f77c501ff3aaa52ecaa5a4c13056ef9c4638f
Michael Gerver <mgerver@ufl.edu>
2020-04-17 22:44:25 -0400

    Try more options for the lunch generator script

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { headerText } from './modules/header';
-import {footerText} from './modules/footer';
-import { lunchOption } from './modules/lunch-generator';
+import { footerText } from './modules/footer';
+// import { lunchOption } from './modules/lunch-generator';
 import './main.css';
 
 class App {
@@ -13,8 +13,8 @@ class App {
         const template = `
         <h1>${headerText.pageTitle}</h1>
         <p>${headerText.pageDescription}</p>
-        <button id="lunch-button"></button>
-        <h3 class="result">${lunchOption.foodItem}</h3>
+        <button id="lunch-button">Generate New Lunch</button>
+        <h3 class="result"></h3>
         <h4>${footerText.footerLabel}</h4>
         <h4>${footerText.lastUpdate}</h4>
         `;
--- src/modules/lunch-generator.js
+++ src/modules/lunch-generator.js
@@ -1,78 +1,83 @@
 class lunchGenerator {
-    constructor(foodItem) {
-        this.foodItem = foodItem;
+    constructor() {
+        this.randomizeLunches();
+        this.showResult();
     }
 
-const btn = document.getElementById('lunch-button');
-btn.addEventListener('click', () => {
+    randomizeLunches() {
+        // Lunch array
+        const lunchArray = [
+            {
+                lunchName: 'Pizza',
+                lunchPic: '../images/pizza.jpg'
+            },
+        
+            {
+                lunchName: 'Noodle Bowl',
+                lunchPic: '../images/noodlebowl.jpg'
+            },
+        
+            {
+                lunchName: 'Burrito',
+                lunchPic: '../images/burrito.jpg'
+            },
+        
+            {
+                lunchName: 'Chicken and Fries',
+                lunchPic: '../images/chicken.jpg'
+            },
+        
+            {
+                lunchName: 'Burger and Fries',
+                lunchPic: '../images/burger.jpg'
+            },
+        
+            {
+                lunchName: 'Crab Cakes',
+                lunchPic: '../images/crabcakes.jpg'
+            },
+        
+            {
+                lunchName: 'Yogurt Parfait',
+                lunchPic: '../images/yogurt.jpg'
+            },
+        
+            {
+                lunchName: 'Grilled Chicken',
+                lunchPic: '../images/grilledchicken.jpg'
+            },
+        
+            {
+                lunchName: 'Boiled Egg',
+                lunchPic: '../images/egg.jpg'
+            },
+        
+            {
+                lunchName: 'Banana Smoothie',
+                lunchPic: '../images/bananasmoothie.jpg'
+            }
+        ]
 
-    // Lunch array
-    const lunchArray = [
-        {
-            lunchName: 'Pizza',
-            lunchPic: 'images/pizza.jpg'
-        },
-    
-        {
-            lunchName: 'Noodle Bowl',
-            lunchPic: 'images/noodlebowl.jpg'
-        },
-    
-        {
-            lunchName: 'Burrito',
-            lunchPic: 'images/burrito.jpg'
-        },
-    
-        {
-            lunchName: 'Chicken and Fries',
-            lunchPic: 'images/chicken.jpg'
-        },
-    
-        {
-            lunchName: 'Burger and Fries',
-            lunchPic: 'images/burger.jpg'
-        },
-    
-        {
-            lunchName: 'Crab Cakes',
-            lunchPic: 'images/crabcakes.jpg'
-        },
-    
-        {
-            lunchName: 'Yogurt Parfait',
-            lunchPic: 'images/yogurt.jpg'
-        },
-    
-        {
-            lunchName: 'Grilled Chicken',
-            lunchPic: 'images/grilledchicken.jpg'
-        },
-    
-        {
-            lunchName: 'Boiled Egg',
-            lunchPic: 'images/egg.jpg'
-        },
-    
-        {
-            lunchName: 'Banana Smoothie',
-            lunchPic: 'images/bananasmoothie.jpg'
-        }
-    ]
+        // Function to shuffle elements within the array
+        let shuffleNum = (i, j, k) => {
+            for (i = lunchArray.length - 1; i > 0; i--) {
+                j = Math.floor(Math.random() * i)
+                k = lunchArray[i]
+                lunchArray[i] = lunchArray[j]
+                lunchArray[j] = k                           
+            };
+            shuffleNum(lunchArray);
+        }; 
 
-    // Function to shuffle elements within the array
-    let shuffleNum = (i, j, k) => {
-        for (i = lunchArray.length - 1; i > 0; i--) {
-            j = Math.floor(Math.random() * i)
-            k = lunchArray[i]
-            lunchArray[i] = lunchArray[j]
-            lunchArray[j] = k
-            // document.getElementById('result').innerHTML = lunchArray[0];                            
-        };
-    }; 
+        
+            };
 
-    // Feeds lunch array to shuffleNum function
-    shuffleNum(lunchArray);
-  });
+        showResult() {
+            let btn = document.getElementById('lunch-button');
+            btn.addEventListener('click', () => {
+            document.getElementById('result').innerHTML = lunchArray[0];
+        }       
+            )}
 }
 
-export const lunchOption = new lunchGenerator (lunchArray[0]);
+// export const lunchOption = new lunchGenerator(lunchArray[0]); 
